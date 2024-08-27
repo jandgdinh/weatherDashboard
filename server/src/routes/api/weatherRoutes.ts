78828466fcea6114b3194072616c4833dc9088ed
John <jandgdinh@gmail.com>
2024-08-26 22:06:54 -0700

    removed todo

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -4,10 +4,7 @@ const router = Router();
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
-// TODO: POST Request with city name to retrieve weather data
 router.post('/:city', async (req: Request, res: Response) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
   try {
     const cityName = req.params.city;
     const weather = await WeatherService.getWeatherForCity(cityName);
@@ -19,7 +16,6 @@ router.post('/:city', async (req: Request, res: Response) => {
   }
 });
 
-// TODO: GET search history
 router.get('/history', async (_req: Request, res: Response) => {
   try {
     const cities = await HistoryService.getCities();
@@ -30,7 +26,6 @@ router.get('/history', async (_req: Request, res: Response) => {
   }
 });
 
-// * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -8,9 +8,6 @@ const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-// TODO: Define route to serve index.html
-
-
 router.get('/', (_req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
